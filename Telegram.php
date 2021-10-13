1360ac2908a835d6988f05abc20f1acb8e2ad1e9
Alex-021 <92319992+Alex-021@users.noreply.github.com>
2021-10-13 06:23:14 +0330

    Add files via upload

---
+++ Telegram.php
@@ -0,0 +1,67 @@
+<?php
+class Telegram {
+ 
+  private static $jsonData;
+
+  public function __construct($json = null) {
+    if ($json != null) {
+      self::$jsonData = json_decode($json);
+    }
+  }
+
+  public function getChatId() {
+    if (isset(self::$jsonData->callback_query->from->id))
+      return self::$jsonData->callback_query->from->id;
+    else
+      return self::$jsonData->message->chat->id;
+  }
+
+  public function getText() {
+    if (isset(self::$jsonData->callback_query->data))
+      return self::$jsonData->callback_query->data;
+    else
+      return self::$jsonData->message->text;
+  }
+
+  public function sendMessage($message) {
+    $message = urlencode($message);
+    $url = "https://api.telegram.org/bot" . _TOKEN;
+    $url .= "/sendMessage?chat_id=" . $this->getChatId();
+    $url .= "&text=" . $message;
+    $results = file_get_contents($url);
+
+    sleep(10);
+
+    $results = json_decode($results);
+    $messageId = $results->message_id;
+    $url = "https://api.telegram.org/bot" . _TOKEN;
+    $url .= "/deleteMessage?chat_id=" . $this->getChatId();
+    $url .= "&message_id=" . $messageId;
+    file_get_contents($url);
+
+  }
+
+  public function setInlineButton() {
+    $keyboardArray =
+      array(
+        array(
+          array("text" => "One", "callback_data" => "1"),
+          array("text" => "Two", "callback_data" => "2")
+        )
+      );
+
+    $inlineKeyboard = array(
+      "inline_keyboard" => $keyboardArray
+    );
+
+    $text = "لطفا یکی از دکمه های زیر را انتخاب کنید :";
+    $text = urlencode($text);
+
+    $inlineKeyboard = json_encode($inlineKeyboard);
+    $url = "https://api.telegram.org/bot" . _TOKEN;
+    $url .= "/sendMessage?chat_id=" . $this->getChatId();
+    $url .= "&text=" . $text;
+    $url .= "&reply_markup=" . $inlineKeyboard;
+    file_get_contents($url);
+  }
+}
--- index.php
+++ index.php
@@ -1,1 +1,32 @@
+<?php
+require_once("Telegram.php");
 
+define("_TOKEN", "1977393633:AAGLFKZYprFmWeTZic-VblpP3Bx_-FAqP9w");
+// define("_ADMIN", "<-YourChatId->");
+
+$json = file_get_contents('php://input');
+
+$tg = new Telegram($json);
+
+$chatId = $tg->getChatId();
+$messageText = $tg->getText();
+
+switch ($messageText) {
+  case "/start":
+    $tg->setInlineButton();
+    break;
+  case "1":
+    $message = "شما بر روی گزینه سمت چپ کلیک کرده اید و مقدار نمایشی آن One و مقدار ارسالی آن 1 است ،";
+    $message .= " این پیام پس از ده ثانیه به طور خودکار پاک می گردد.";
+    $tg->sendMessage($message);
+    break;
+  case "2":
+    $message = "شما بر روی گزینه سمت چپ کلیک کرده اید و مقدار نمایشی آن Two و مقدار ارسالی آن 2 است ،";
+    $message .= " این پیام پس از ده ثانیه به طور خودکار پاک می گردد.";
+    $tg->sendMessage($message);
+    break;
+  default:
+    $message = "دستور ناشناخته است !";
+    $tg->sendMessage($message);
+    break;
+}
